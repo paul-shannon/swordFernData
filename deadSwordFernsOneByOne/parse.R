9558e6137105687737a573a2ed0d5db20b6f607f
paul-shannon <paul.thurmond.shannon@gmail.com>
2020-02-09 11:22:51 -0800

    added locations from 25jan and 02feb

--- deadSwordFernsOneByOne/parse.R
+++ deadSwordFernsOneByOne/parse.R
@@ -25,39 +25,43 @@ test_toDecimalLoc <- function()
 #f <- "Solocator.2019-12-29.15.03.57.csv"
 #f.in <- "Solocator.2020-01-06.06.22.23.csv"
 #f.out <- "locations-05jan2020.js"
-
-f.in <- "Solocator.2020-01-12.19.20.26.csv"
-f.out <- "locations-12jan2020.js"
-
+# f.in <- "Solocator.2020-01-12.19.20.26.csv"
+# f.out <- "locations-12jan2020.js"
 #f <- "Solocator.2020-01-12.19.20.26.csv"
 #f <- "19jan2020.csv"
 
-tbl <- read.table(f.in, sep=",", as.is=TRUE, header=TRUE,nrow=-1, quote=NULL, fill=TRUE)
-dim(tbl)
-lat <- tbl$Latitude
-lat <- as.numeric(sub("°", "", lat))
-long <- tbl$Longitude
-long <- as.numeric(sub("°", "", long))
+f.in <- "download-02feb2020.csv"
+f.out <- "locations-02feb020.csv"
 
-lats <- jitter(lat, amount=0.0005)
-longs <- jitter(long, amount=0.0005)
+run <- function()
+{
+   tbl <- read.table(f.in, sep=",", as.is=TRUE, header=TRUE,nrow=-1, quote=NULL, fill=TRUE)
+   dim(tbl)
+   lat <- tbl$Latitude
+   lat <- as.numeric(sub("°", "", lat))
+   long <- tbl$Longitude
+   long <- as.numeric(sub("°", "", long))
+
+   lats <- jitter(lat, amount=0.0005)
+   longs <- jitter(long, amount=0.0005)
 
 # lats <- unlist(lapply(lat, toDecimalLoc))
-# longs <- -1 * unlist(lapply(long, toDecimalLoc))
+   # longs <- -1 * unlist(lapply(long, toDecimalLoc))
 
-js.file <- file(f.out)
+   js.file <- file(f.out)
 
-line.count <- length(lats) + 3;
-lines <- vector("character", line.count)
-lines[1] <- "var locations = [";
+   line.count <- length(lats) + 3;
+   lines <- vector("character", line.count)
+   lines[1] <- "var locations = [";
 
-out.count <- length(lats)
+   out.count <- length(lats)
 
-for(i in seq_len(out.count)){
-   text <- sprintf("   {lat: %f, lng: %f},", lats[i], longs[i])
-   lines[i+1] <- text
+   for(i in seq_len(out.count)){
+      text <- sprintf("   {lat: %f, lng: %f},", lats[i], longs[i])
+      lines[i+1] <- text
+      }
+   lines[i+1] <- "];"
+
+   writeLines(lines, js.file)
+   close(js.file)
    }
-lines[i+1] <- "];"
-
-writeLines(lines, js.file)
-close(js.file)
