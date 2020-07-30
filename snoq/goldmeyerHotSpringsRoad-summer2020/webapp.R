2224f072b12143fbe5ec3a5eff69261789032371
paul-shannon <paul.thurmond.shannon@gmail.com>
2020-07-30 13:57:46 -0700

    ready for mark boyar

--- snoq/goldmeyerHotSpringsRoad-summer2020/webapp.R
+++ snoq/goldmeyerHotSpringsRoad-summer2020/webapp.R
@@ -3,27 +3,32 @@ library(leaflet)
 library(plotKML)
 #----------------------------------------------------------------------------------------------------
 library(yaml)
-x <-  yaml.load(readLines("site.yaml"))
 
-points <- x$points
+config <-  yaml.load(readLines("site.yaml"))
+
+points <- config$points
 tbl <- do.call(rbind, lapply(points, as.data.frame))
 tbl$name <- as.character(tbl$name)
 tbl$details <- as.character(tbl$details)
-center.lat <- min(tbl$lat) + ((max(tbl$lat) - min(tbl$lat))/2)
-center.lon <- min(tbl$lon) + ((max(tbl$lon) - min(tbl$lon))/2)
+# center.lat <- min(tbl$lat) + ((max(tbl$lat) - min(tbl$lat))/2)
+# center.lon <- min(tbl$lon) + ((max(tbl$lon) - min(tbl$lon))/2)
+tbl$label <- tbl$name; # with(tbl, sprintf("%s %s (%s)", name, date, observer))
+groups <- sort(unique(tbl$group))
 #----------------------------------------------------------------------------------------------------
 ui <- fluidPage(
 
-  titlePanel("Sword Fern Die-off Reports from the Snoqualmie Middle Fork"),
+  titlePanel(config$title),
   sidebarLayout(
       sidebarPanel(
-         selectInput("siteSelector", "Select Site", c("", as.character(1:nrow(tbl)))),
+         #includeCSS("map.css"),
+         selectInput("siteSelector", "Select Site", c("-", sort(tbl$label))),
          actionButton("fullViewButton", "Full View"),
-
+         br(), br(), br(),
+         checkboxGroupInput("groupsSelector", "Category", choices = groups, selected = groups),
          width=2
          ),
       mainPanel(
-          tags$style(type = "text/css", "#map {height: calc(100vh - 80px) !important;}"),
+          tags$style(type = "text/css", "#map {height: calc(100vh - 120px) !important;}"),
           tabsetPanel(type="tabs", id="mapTabs",
                       tabPanel(title="Map", value="mapTab", leafletOutput("map")),
                       tabPanel(title="Site Details", value="siteDetailsTab",
@@ -38,20 +43,24 @@ server <- function(input, output, session) {
   observeEvent(input$fullViewButton, ignoreInit=TRUE, {
      isolate({
         leafletProxy('map') %>%
-           setView(lng=center.lon, lat=center.lat, zoom=14)
+           setView(lng=config$centerLon, lat=config$centerLat, zoom=config$initialZoom)
         })
-
+      updateTabsetPanel(session, "mapTabs", selected="mapTab")
       })
 
   observe({
      req(input$siteSelector)
-     siteNumber <- as.integer(input$siteSelector)
-     lat <- tbl[siteNumber, "lat"]
-     lon <- tbl[siteNumber, "lon"]
-     isolate({
-        leafletProxy('map') %>%
-            setView(lon, lat, zoom=18)
-        })
+     siteID <- input$siteSelector
+     printf("zoom to '%s'", siteID)
+     tbl.sub <- subset(tbl, label==siteID)
+     if(nrow(tbl.sub) == 1){
+        lat <- tbl.sub$lat
+        lon <- tbl.sub$lon
+        isolate({
+          leafletProxy('map') %>%
+              setView(lon, lat, zoom=16)
+          })
+         } # nrow
     })
 
 
@@ -70,34 +79,22 @@ server <- function(input, output, session) {
      insertUI("#temporaryDiv", "beforeEnd",
               div(id="detailsDiv", includeHTML(details.url)))
      updateTabsetPanel(session, "mapTabs", selected="siteDetailsTab")    # provided by shiny
-
-     #insertUI(
-     #    selector = '#foo',
-     #    ui = includeHTML(details.url))
      })
 
-#  output$foo <- renderUI({
-#     #req(input$map_marker_click)
-#     event <- input$map_marker_click
-#     print(names(event))
-#     print(event)
-#     lat <- event$lat
-#     lon <- event$lng
-#     printf("lat: %f  lon: %f", lat, lon)
-#     details.url <- subset(tbl, name==event$id)$details
-#     print(details.url)
-#     "<h4>foo</h4>"
-#     })
-
   output$map <- renderLeaflet({
-      options = leafletOptions(minZoom=0, maxZoom=22, maxNativeZoom=18)
-      options.tile <- tileOptions(minZoom=0, maxZoom=22, maxNativeZoom=18)
-      leaflet(options) %>%
-      addTiles(options=options.tile) %>%
-      setView(center.lon, center.lat, zoom=14) %>%
-      addCircleMarkers(tbl$lon, tbl$lat, label=tbl$name, color=tbl$color, radius=tbl$radius, layerId=tbl$name) %>%
-      addScaleBar()
-      })
+     currentGroups <- input$groupsSelector
+     tbl.sub <- subset(tbl, group %in% currentGroups)
+     options = leafletOptions(minZoom=0, maxZoom=22, maxNativeZoom=18)
+     options.tile <- tileOptions(minZoom=0, maxZoom=22, maxNativeZoom=18)
+     map <- leaflet(options) %>%
+         addTiles(options=options.tile) %>%
+         setView(config$centerLon, config$centerLat, zoom=config$initialZoom) %>%
+         addScaleBar()
+     if(nrow(tbl.sub) > 0)
+        map <- addCircleMarkers(map, tbl.sub$lon, tbl.sub$lat, label=tbl.sub$label, color=tbl.sub$color, radius=tbl.sub$radius, layerId=tbl.sub$name)
+     map
+     })
 }
 #----------------------------------------------------------------------------------------------------
+with(config, printf("config:  %f   %f   %d", centerLon, centerLat, initialZoom))
 runApp(shinyApp(ui, server), host="0.0.0.0", port=6789)
